81d2ba58178d41083cf71edfe9cc20367be79099
johanneshagl7-jpg <johanneshagl7@gmail.com>
2025-09-26 15:02:18 +0200

    Add files via upload

---
+++ src/TerrazzoCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TerrazzoCard() {
+  return (
+    <div className="p-4 bg-white rounded-2xl shadow">
+      <h2 className="text-xl font-semibold mb-2">Terrazzo Applikationskarten</h2>
+      <p>Standardfaktor: <b>160 m³/h pro 1000 rpm</b></p>
+    </div>
+  );
+}
